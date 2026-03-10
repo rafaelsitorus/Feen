a3448b7e55258a38151dc996976ae116b077d572
rafaelsitorus <rafaelraja.sitorus@gmail.com>
2026-03-10 14:49:37 +0700

    Initial Commit

---
+++ Feen/FeenApp.swift
@@ -0,0 +1,17 @@
+//
+//  FeenApp.swift
+//  Feen
+//
+//  Created by Pangihutan Sitorus on 10/03/26.
+//
+
+import SwiftUI
+
+@main
+struct FeenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
