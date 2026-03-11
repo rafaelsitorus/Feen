b96d4a1d874902ae733592142835a366cfdd7dd7
fidelcavell <fidelcavell.learning@gmail.com>
2026-03-11 13:38:07 +0700

    Initial Commit

---
+++ feen/feenApp.swift
@@ -0,0 +1,17 @@
+//
+//  feenApp.swift
+//  feen
+//
+//  Created by Fidel Fausta Cavell on 11/03/26.
+//
+
+import SwiftUI
+
+@main
+struct feenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
