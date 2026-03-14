d9857fbcd5fccb2a2aa8dea7422c345f1906299a
rafaelsitorus <rafaelraja.sitorus@gmail.com>
2026-03-14 22:12:15 +0700

    chore: Utils

---
+++ Feen/Utils/SourceOfIncomeEnum.swift
@@ -0,0 +1,17 @@
+//
+//  SourceOfIncomeEnum.swift
+//  feen
+//
+//  Created by Fidel Fausta Cavell on 12/03/26.
+//
+
+import Foundation
+
+/// Enum dari SourceOfIncome - sudah diganti dengan file di Models/SourceOfIncome (Optional)
+enum SourceOfIncomeEnum: String, Codable {
+    case salary
+    case investment
+    case parentalSupport
+    case pension
+    case other
+}
